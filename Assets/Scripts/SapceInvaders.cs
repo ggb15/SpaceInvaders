c8ea900c80e724bc13cffe35e5fa6c6061b20f8c
ggb15 <guilherme.g.brandao@gmail.com>
2021-03-30 16:59:35 +0100

    Initial commit

---
+++ Assets/Scripts/SapceInvaders.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SapceInvaders : MonoBehaviour
+{
+    [SerializeField]
+    GameObject invasorA;
+
+    [SerializeField]
+    GameObject invasorB;
+
+    [SerializeField]
+    GameObject invasorC;
+
+    [SerializeField]
+    GameObject[] invasores;
+
+    [SerializeField]
+    int nInvasores = 7;
+
+    [SerializeField]
+    float xMin = -3f;
+
+    [SerializeField]
+    float yMin = 0.5f;
+
+    [SerializeField]
+    float xInc = 1f;
+
+    [SerializeField]
+    float yInc = 0.5f;
+
+
+    void Awake()
+    {
+        float y = yMin;
+
+        for(int line = 0; line <5; line++)
+        {
+            GameObject invader = null;
+
+            if(line == 0 || line == 1)
+            {
+                invader = invasorA;
+            }else if (line == 2 || line == 3)
+            {
+                invader = invasorB;
+            }else if( line == 4)
+            {
+                invader = invasorC;
+            }
+
+            
+            float x = xMin;
+            for(int i = 1; i <=nInvasores; i++)
+            {
+                GameObject newInvader = Instantiate(invader, transform);
+                newInvader.transform.position = new Vector3(x, y, 0f);
+                x += xInc;
+            }
+            y += yInc;
+                   
+
+        }
+    }
+        
+    
+
+
+}
